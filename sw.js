d666d64b9b2ef4e1d522107c7bc9b0094250930c
wwlee94 <wwlee94@naver.com>
2020-07-29 23:43:21 +0900

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-36fb9e3ccb7458e3f9b6.js"
+    "url": "webpack-runtime-f76f3699c173fe70b585.js"
   },
   {
     "url": "framework-59a9ef51c55d9b62f7de.js"
@@ -45,7 +45,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a1d70af24f3086d6db640f7190c07ff2"
+    "revision": "5a1a49e05e587cf92f92cf30684a3562"
   },
   {
     "url": "manifest.webmanifest",
